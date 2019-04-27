2ae18756655b3f9760d5febb891f21c34e554e42
shreya0ag <shreya0swami@gmail.com>
2019-04-27 17:22:17 +0530

    completing chapter 7

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,27 @@
 class UsersController < ApplicationController
-	attr_accessor :name, :email
+	# attr_accessor :name, :email
   def new
+  	@user = User.new
   end
+
+  def show
+   @user = User.find(params[:id])
+  end
+
+  def create
+  	@user = User.new(user_params)
+  	
+  	if @user.save
+  		redirect_to @user
+      flash[:success] = "Welcome #{@user.name}"
+  	else
+  		render 'new'
+  	end
+  end
+
+
+private
+def user_params
+	params.require(:user).permit(:name, :email, :password, :password_confirmation)
+end
 end
